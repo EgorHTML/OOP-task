9434baf87e6daa05552ca46bbccbcea45213801d
Egor <jdmjapanese69@gmail.com>
2024-05-14 16:29:10 +0300

    Добавьте файлы проекта.

---
+++ ASYNCLABAOOP/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    async static public Task Main()
+    {
+        HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "C# agent");
+        string res = await getVacanciesAsync(1);
+        Console.WriteLine(res);
+
+        string res2 = await getLanguagesAsync(1);
+        Console.WriteLine(res2);
+
+        async Task<string> getVacanciesAsync(int page)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            HttpResponseMessage result =await client.GetAsync("https://api.hh.ru/vacancies?page="+ page);
+            watch.Stop();
+            Console.WriteLine($"Асинхронный вызов getVacanciesAsync: {watch.ElapsedMilliseconds} ms");
+
+            if (!result.IsSuccessStatusCode) throw new Exception("vacancies Async error");
+
+            string vacancies = await result.Content.ReadAsStringAsync();
+            return vacancies;
+        }
+
+        async Task<string> getLanguagesAsync(int page)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            HttpResponseMessage result = await client.GetAsync("https://api.hh.ru/languages?page=" + page);
+            watch.Stop();
+            Console.WriteLine($"Асинхронный вызов getVacanciesAsync: {watch.ElapsedMilliseconds} ms");
+
+            if (!result.IsSuccessStatusCode) throw new Exception("vacancies Async error");
+
+            string vacancies = await result.Content.ReadAsStringAsync();
+            return vacancies;
+        }
+
+       
+    }
+}
---
+++ ConsoleAppSync/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+class Program
+{
+    static public void Main()
+    {
+        HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "C# agent");
+        string res = getVacanciesSync(2);
+        Console.WriteLine(res);
+
+        string getVacanciesSync(int page)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            HttpResponseMessage result =  client.GetAsync("https://api.hh.ru/vacancies?page=" + page).Result;
+            watch.Stop();
+            Console.WriteLine($"Cинхронный вызов getVacanciesSync: {watch.ElapsedMilliseconds} ms");
+
+            if(!result.IsSuccessStatusCode) throw new Exception("vacancies sync error");
+
+            string vacancies =  result.Content.ReadAsStringAsync().Result;
+            return vacancies;
+        }
+    }
+}
